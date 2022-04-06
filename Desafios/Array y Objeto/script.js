4b74a80b3de58bf408406bf620d23d3d657f7c37
unknown <mauro4889@gmail.com>
2022-04-06 11:47:00 -0300

    Primer subida al repo

---
+++ Desafios/Array y Objeto/script.js
@@ -0,0 +1,5 @@
+let dias= "lunes, martes, miércoles, jueves, viernes";
+
+console.log(`La longitud del string es: ${dias.length}`);
+console.log(dias.toUpperCase());
+console.log(dias.replaceAll(",","-"));
